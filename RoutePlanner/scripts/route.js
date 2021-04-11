34179525257f9c4f419d5afc0966d336d9732fba
mwiiwm <77498225+mwiiwm@users.noreply.github.com>
2021-04-11 11:32:02 -0400

    radio fix, jquery, map style ajax, and stuffs

--- RoutePlanner/scripts/route.js
+++ RoutePlanner/scripts/route.js
@@ -13,19 +13,36 @@ $(document).ready(function() {
             center: home,
             zoom: 15,
             disableDefaultUI: true,
+            mapTypeControlOptions: {
+                mapTypeIds: ['style'],
+              },
         });
+        $.getJSON('styles/map.JSON', function(data) {
+            map.mapTypes.set('style', new google.maps.StyledMapType(data));
+            map.setMapTypeId('style');
+        });
+
         start = new google.maps.Marker({
             position: home,
             map: map,
             draggable: true,
             crossOnDrag: true,
         });
+        var startInfo = new google.maps.InfoWindow({
+          content: '<h1><b>Start</b></h1><br>' +
+                   '<p>This is the start of the route, it defaults to your home. ' +
+                   'Click a location on the map to set your destination. After doing so ' +
+                   'you can drag the path and markers to alter the route.</p>'
+        });
+        startInfo.open(map, start);
+
         end = new google.maps.Marker({
             position: null,
             map: map,
             draggable: true,
             crossOnDrag: true,
         });
+        var endInfo;
 
         const paths = new google.maps.BicyclingLayer();
         paths.setMap(map);
@@ -42,6 +59,11 @@ $(document).ready(function() {
         setEnd = map.addListener('click', function(e) {
             end.setPosition(e.latLng);
             changeRoute(start, end, dirService, dirRender);
+            endInfo = new google.maps.InfoWindow({
+              content: '<h1><b>Destination<b></h1>',
+            })
+            endInfo.open(map, end);
+            startInfo.setContent('<h1><b>Start</b></h1>');
             setEnd.remove();
         });
 
@@ -49,14 +71,42 @@ $(document).ready(function() {
             changeRoute(start, end, dirService, dirRender);
         });
 
+        start.addListener('click', function() {
+          startInfo.open(map, start);
+        });
+
         end.addListener('dragend', function() {
             changeRoute(start, end, dirService, dirRender);
+        });
+
+        end.addListener('click', function() {
+          endInfo.open(map, end);
+        });
+
+        $('input[type=radio]').change(function() {
+            updateInfo(dirRender.getDirections());
+        });
+
+        $('input[type=text]').change(function() {
+            updateInfo(dirRender.getDirections());
         });
 
         dirRender.addListener('directions_changed', function() {
             updateInfo(dirRender.getDirections());
         });
+
+        $('#save').click(function() {
+            if (!dirRender.getDirections()) { return; }
+            console.log(dirRender.getDirections().geocoded_waypoints);
+            var waypoints = dirRender.getDirections().geocoded_waypoints;
+        })
     };
+
+    $(document).keypress(function(event) {
+        if (event.which == '13') {
+            event.preventDefault();
+        }
+    });
 
     function changeRoute(start, end, service, render) {
         let options = {
@@ -68,12 +118,14 @@ $(document).ready(function() {
             if (status === 'OK' && result) {
                 render.setDirections(result);
             } else {
-                alert('Rendering the route caused an error.');
+                alert('An error was caused by rendering the route.');
             }
         });
     }
 
-    function updateInfo(route, mode) { 
+    function updateInfo(route) { 
+        if (!route) { return; }
+
         let distance = getDistance(route);
         document.getElementById('dist').innerHTML = `Distance: ${distance.toFixed(2)} km`;
 
@@ -81,13 +133,11 @@ $(document).ready(function() {
         let duration = distance / speed;
         document.getElementById('duration').innerHTML = `Duration: ${~~duration} minutes and ${~~((duration - ~~duration) * 60)} seconds`;
         
-        let calories = getCalories(mode, duration, speed * 60.0);
+        let calories = getCalories(duration, speed * 60.0);
         document.getElementById('cals').innerHTML = `Calories: ${Math.round(calories)} cal`;
     }
 
     function getDistance(route) {
-        if (!route) { return; }
-
         let distance = 0;
         for (let i = 0; i < route.routes[0].legs.length; i++) {
             distance += route.routes[0].legs[i].distance.value;
@@ -96,7 +146,7 @@ $(document).ready(function() {
         return distance / 1000;
     }
 
-    function getCalories(mode, duration, speed) {
+    function getCalories(duration, speed) {
         let MET;
         if (document.getElementById('bicycle').checked) { MET = getBikeMET(speed); } 
         else if (document.getElementById('on-foot').checked) { MET = getFootMET(speed); }
