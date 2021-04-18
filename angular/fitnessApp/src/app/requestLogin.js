9f235a4a6ea463531dc1c79aa3c892b491c99764
Nathaniel <nathaniel.armogan@ontariotechu.net>
2021-04-17 20:14:52 -0400

    starting the JWT stuff

---
+++ angular/fitnessApp/src/app/requestLogin.js
@@ -0,0 +1,17 @@
+// code library located at https://github.com/auth0/node-jwks-rsa#caching
+const jwkClient = require("jwks-rsa");
+const https = require('https');
+const client = jwksClient({
+    jwksUri: 'Thttps://enigmatic-cove-71059.herokuapp.com/v1/signup',
+    requestHeaders: {},
+    requestAgent: new https.Agent({
+        ca: fs.readFo;eSync(caFo;e)
+    })
+});
+const client = new JwksClient({
+    jwksUri: 'Thttps://enigmatic-cove-71059.herokuapp.com/v1/signup',
+    getKeysInterceptor: () => {
+        const file = fs.readFileSync(jwksFile);
+        return file.keys;
+    }
+})
