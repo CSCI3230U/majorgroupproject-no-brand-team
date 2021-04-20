707589b50f527e75b91d03ec1252a6c8f1518b55
mwiiwm <77498225+mwiiwm@users.noreply.github.com>
2021-04-20 12:33:05 -0400

    full functionality, css, pain

--- angular/fitnessApp/src/app/components/route/route.component.ts
+++ angular/fitnessApp/src/app/components/route/route.component.ts
@@ -49,10 +49,229 @@ export class RouteComponent implements OnInit {
                 mapTypeIds: ['style'],
             },
         });
-        $.getJSON('assets/map.json', function(data: any) {
+        /*$.getJSON('map.json', function(data: any) {
             map.mapTypes.set('style', new google.maps.StyledMapType(data));
             map.setMapTypeId('style');
-        });
+        });*/
+
+        var mapData = `[
+            {
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#ebe3cd"
+                }
+              ]
+            },
+            {
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#523735"
+                }
+              ]
+            },
+            {
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                {
+                  "color": "#f5f1e6"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#c9b2a6"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative.land_parcel",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#dcd2be"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative.land_parcel",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#ae9e90"
+                }
+              ]
+            },
+            {
+              "featureType": "landscape.natural",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#dfd2ae"
+                }
+              ]
+            },
+            {
+              "featureType": "poi",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#dfd2ae"
+                }
+              ]
+            },
+            {
+              "featureType": "poi",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#93817c"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.park",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#a5b076"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.park",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#447530"
+                }
+              ]
+            },
+            {
+              "featureType": "road",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#f5f1e6"
+                }
+              ]
+            },
+            {
+              "featureType": "road.arterial",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#fdfcf8"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#f8c967"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#e9bc62"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway.controlled_access",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#e98d58"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway.controlled_access",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#db8555"
+                }
+              ]
+            },
+            {
+              "featureType": "road.local",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#806b63"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.line",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#dfd2ae"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.line",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#8f7d77"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.line",
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                {
+                  "color": "#ebe3cd"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.station",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#dfd2ae"
+                }
+              ]
+            },
+            {
+              "featureType": "water",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#b9d3c2"
+                }
+              ]
+            },
+            {
+              "featureType": "water",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#92998d"
+                }
+              ]
+            }
+          ]`;
+
+        map.mapTypes.set('style', new google.maps.StyledMapType(JSON.parse(mapData)));
+        map.setMapTypeId('style');
     
         start = new google.maps.Marker({
             position: home,
@@ -137,12 +356,11 @@ export class RouteComponent implements OnInit {
             if (name == null || name == '') { return; }
     
             let route = getRouteObj(name, mode);
-            console.log(route)
             $.ajax({
                 type: 'POST',
                 contentType: "application/json",
-                url: 'https://enigmatic-cove-71059.herokuapp.com/v1/route',
-                headers: {"Authorization": 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjE4OTMyNTE3LCJleHAiOjE2MTkwMTg5MTd9.ds-rOFpI4rnTaS4cGU64F4VPyqftF9Grka88WICqdno'},
+                url: 'https://enigmatic-cove-71059.herokuapp.com/v1/route/',
+                headers: {"Authorization": 'Bearer ' + token},
                 data: JSON.stringify(route),
                 success: function() {
                     let found = false;
@@ -157,27 +375,23 @@ export class RouteComponent implements OnInit {
                         if (found) {
                             let routeBoxes = $('#routes .box');
                             for (let i = 0; i < routeBoxes.length; i++) {
-                                console.log(routeBoxes[i].firstChild.innerHTML, name);
                                 if (name == routeBoxes[i].firstChild.innerHTML) {
-                                    routeBoxes[i].lastChild.setAttribute('src', `svg/${mode}.svg`);
+                                    routeBoxes[i].lastChild.setAttribute('ng-src', `svg/${mode}.svg`);
                                     break;
                                 }
                             }
                         } else {
                             makeRouteBox(name, mode);
                             $.ajax({
-                                url: 'https://enigmatic-cove-71059.herokuapp.com/v1/route/',
+                                url: 'https://enigmatic-cove-71059.herokuapp.com/v1/route/' + name,
                                 type: 'GET',
                                 headers: {"Authorization": 'Bearer ' + token},
                                 success: function(res:any) {
-                                    routes.push(res);
+                                    routes.push(res.result);
                                 }
                             });
                         }
                     }
-                },
-                error: function(err: string) {
-                    console.log(err)
                 }
             });
         });
@@ -206,7 +420,7 @@ export class RouteComponent implements OnInit {
                                 routesElement.removeChild(routesElement.childNodes[i]);
                                 for (let j = 0; j < routes.length; j++) {
                                     if (routes[j].name = nameElement.value) {
-                                        routes = routes.splice(j, 1);
+                                        routes.splice(j, 1);
                                         break;
                                     }
                                 }
@@ -233,8 +447,13 @@ export class RouteComponent implements OnInit {
         });
     
         $('#routes').on('click', '.box', function(e:any) {
-            let element = $(e.target);
-            let name = element.firstChild.innerHTML;
+            let element = e.target;
+            let name;
+            if (element.className == 'route-name') {
+                name = element.innerHTML;
+            } else {
+                name = element.firstChild.innerHTML;
+            }
             for (let i = 0; i < routes.length; i++) {
                 if (name === routes[i].name) {
                     setRouteInfo(name, routes[i].mode, parseFloat(routes[i].speed));
@@ -331,14 +550,14 @@ export class RouteComponent implements OnInit {
     
         function makeRouteBox(name:any, mode:any) {
             let routes = document.getElementById('routes')! as HTMLElement;
-            let div = document.createElement('div');
+            let div = document.createElement('div')! as HTMLElement;
             div.setAttribute('class', 'box');
-            let strong = document.createElement('b');
+            let strong = document.createElement('b')! as HTMLElement;
             strong.setAttribute('class', 'route-name');
             strong.appendChild(document.createTextNode(`${name}`));
             div.appendChild(strong);
-            let icon = document.createElement('img');
-            icon.setAttribute('src', `svg/${mode}.svg`);
+            let icon = document.createElement('img')! as HTMLElement;
+            icon.setAttribute('ng-src', `/svg/${mode}.svg`);
             icon.setAttribute('class', `route-icon`);
             div.appendChild(icon);
             routes.appendChild(div);
